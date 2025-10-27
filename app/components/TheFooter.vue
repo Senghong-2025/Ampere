138cd225ae19287648dadd76bca201be59c0e1fd
SenghongH <senghong.hang@techbodia.com>
2025-10-27 20:55:30 +0700

    update created

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-gray-100 py-4 border-t border-gray-200">
         <p class="text-center text-gray-400 text-sm">
-            &copy; 2025 Ampere. All rights reserved.<br> Founded by Seng Hong
+            &copy; 2025 Ampere. All rights reserved.<br> Created by Seng Hong
         </p>
     </div>
 </template>
