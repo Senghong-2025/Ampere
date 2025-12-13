ae19aa2e2da9a37d3f20f9c641474a3d7ee763d7
Senghong03 <senghong.hang@techbodia.com>
2025-12-13 16:53:57 +0700

    remove unsed code

--- app/pages/load.vue
+++ app/pages/load.vue
@@ -23,44 +23,4 @@ const { loadRoutes } = useNavbar();
 onMounted(() => {
     navigateTo("/load/details");
 })
-// async function generatePDF() {
-//     const { jsPDF } = await import("jspdf");
-//     const { autoTable } = await import("jspdf-autotable");
-//     const doc = new jsPDF({
-//         orientation: "portrait",
-//         unit: "pt",
-//         format: "a4",
-//     });
-
-//     loads.forEach((load, loadIndex) => {
-//         doc.setFontSize(18);
-//         doc.text(`Electrical Load Report - Home ID: ${load.homeId} - Date: ${load.date}`, 40, 40);
-
-//         const tableData = load.data.map(room => [
-//             room.roomName ?? '',
-//             room.connectedLoadKW ?? '',
-//             room.voltage ?? '',
-//             room.current ?? '',
-//             room.powerFactor ?? '',
-//             (room.devices && Array.isArray(room.devices))
-//                 ? room.devices.map(item => item.hoursUsed ?? '').join(', ')
-//                 : '',
-//         ]);
-
-//         autoTable(doc, {
-//             startY: 60,
-//             head: [['Room', 'Load (kW)', 'Voltage (V)', 'Current (A)', 'Power Factor', 'Devices']],
-//             body: tableData,
-//             theme: 'grid',
-//             styles: { fontSize: 10 },
-//             // headStyles: { fillColor: [500, 240, 240] },
-//         });
-
-//         if (loadIndex < loads.length - 1) {
-//             doc.addPage();
-//         }
-//     });
-
-//     doc.save(`Electrical_Load_Report_${new Date().toISOString()}.pdf`);
-// }
 </script>
